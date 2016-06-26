f348e072875ac7157864af3b65faddc41145f7de
Jonathan Hedstrom <delnorte91@gmail.com>
2016-06-25 19:22:16 -0500

    Checkpoint 27 complete

---
+++ Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Jonathan on 6/25/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+@end
